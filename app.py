c729dc3a3dd9b051d99f870b691dc3c8244e3270
DzramadoSolomon <solomonkendzramado@gmail.com>
2025-08-11 00:58:29 +0000

    Update app.py

--- app.py
+++ app.py
@@ -253,5 +253,5 @@ def health_check():
 
 if __name__ == '__main__':
     load_model()
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8000))
     app.run(host='0.0.0.0', port=port, debug=True)
